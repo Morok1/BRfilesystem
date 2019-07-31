8be43bf36909a6c255f0fa4c2af6d91dec9e24ae
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-07-31 14:50:37 +0300

    simple model

---
+++ src/main/java/model/Catalogue.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Catalogue {
+    private String name;
+
+}
+
---
+++ src/main/java/model/File.java
@@ -0,0 +1,8 @@
+package model;
+
+public class File {
+    private String name;
+    private File parent;
+    private int size;
+
+}
